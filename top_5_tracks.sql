c07908578022a94b4385a6ef9b77482112d50b50
jakefroeb <jake.herman.froeb@gmail.com>
2021-05-25 10:25:46 -0500

    done

--- top_5_tracks.sql
+++ top_5_tracks.sql
@@ -2,6 +2,6 @@
 SELECT SUM(Quantity) as TracksPurchased, TrackId
 FROM InvoiceLine
 JOIN Invoice on Invoice.InvoiceId = InvoiceLine.InvoiceId
-Group By InvoiceLine.InvoiceId
+Group By InvoiceLine.TrackId
 Order By TracksPurchased DESC LIMIT 5
 
